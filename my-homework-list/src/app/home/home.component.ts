36b7c946c225d9291c9d78a27c097389464fde40
DazhyGM <kevinsabogal24@gmail.com>
2024-11-08 10:16:29 -0500

    se finaliza la ultima diapositiva del angular

--- my-homework-list/src/app/home/home.component.ts
+++ my-homework-list/src/app/home/home.component.ts
@@ -9,27 +9,32 @@ import {HousingLocation} from '../housing-location';
   imports: [CommonModule, HousingLocationComponent],
   template: `
   <section>
-    <form>
-      <input type="text" placeholder="Filter by city" />
-      <button class="primary" type="button">Search</button>
+    <form class="row">
+
+      <div class="input-group mb-3">
+
+          <input type="text" class="form-control" placeholder="Filter by city" aria-label="Filter by city">
+          <button class="btn btn-primary" type="button">Search</button>
+      </div>
     </form>
-  </section>
-  <section class="results">
-    <app-housing-location></app-housing-location>
-  </section>
+  <section>
+      <section class="results">
+          <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
+      </section>
   `,
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
+
 export class HomeComponent {
-  readonly baseURL = 'https://angular.dev/assets/images/tutorials/common';
-  housingLocation: HousingLocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test City',
-    state: 'ST',
-    photo: `${this.baseURL}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  housingLocation = {
+      id: 9999,
+      name: 'Test Home',
+      city: 'Test City',
+      state: 'ST',
+      photo: `${this.baseUrl}/example-house.jpg`,
+      availableUnits: 99,
+      wifi: true,
+      laundry: false,
   };
 }
